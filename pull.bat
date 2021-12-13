ad5b76841e26d44b0f75e4eecb6f72aff71fb4b3
Brian Reynolds <brian@brianreynolds.net>
2021-12-13 16:56:26 -0500

    update mod name

--- pull.bat
+++ pull.bat
@@ -1,4 +1,4 @@
-7z x "C:\Users\Brian Reynolds\Documents\Gaming & German Game Rules\Vassal\Almoravid_%1.vmod" buildfile.xml -aoa
-7z x "C:\Users\Brian Reynolds\Documents\Gaming & German Game Rules\Vassal\Almoravid_%1.vmod" moduledata -aoa
+7z x "C:\Users\Brian Reynolds\Documents\Gaming & German Game Rules\Vassal\Nevsky_%1.vmod" buildfile.xml -aoa
+7z x "C:\Users\Brian Reynolds\Documents\Gaming & German Game Rules\Vassal\Nevsky_%1.vmod" moduledata -aoa
 
 
--- pullall.bat
+++ pullall.bat
@@ -1,3 +1,3 @@
-7z x "C:\Users\Brian Reynolds\Documents\Gaming & German Game Rules\Vassal\Almoravid_%1.vmod" * -aoa
+7z x "C:\Users\Brian Reynolds\Documents\Gaming & German Game Rules\Vassal\Nevsky_%1.vmod" * -aoa
 
 
